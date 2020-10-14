887ac43d5dc9787cf2b4f9d0d200c3e7f0ef72d1
Quang Dang <quangdangfit@gmail.com>
2020-10-14 15:30:38 +0700

    update dig api

--- app/api/dig.go
+++ app/api/dig.go
@@ -3,8 +3,6 @@ package api
 import "go.uber.org/dig"
 
 func Inject(container *dig.Container) error {
-	_ = container.Provide(NewRouting)
-	_ = container.Provide(NewOutMsg)
 	_ = container.Provide(NewAction)
 
 	return nil
